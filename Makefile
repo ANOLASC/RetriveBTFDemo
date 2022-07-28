9b9ae657a9fe02a6e2d0d3247b9f6d3fc8adcb3d
ANOLASC <22795996+ANOLASC@users.noreply.github.com>
2022-07-29 00:11:58 +0800

    better print

--- Makefile
+++ Makefile
@@ -1,31 +1,20 @@
-CLANG = clang
+GCC = gcc
+TARGET = btfdemo
 
-BPFCODE = bpf_program
+clean:
+	rm $(TARGET)
 
-BPFTOOLS = /kernel-src/samples/bpf
-BPFLOADER = $(BPFTOOLS)/bpf_load.c
+INCLUDE += -I./libbpf/include/uapi
+LIBRARY_PATH += -L./libbpf/src/
+DYNSO += -lbpf
+DYNSO += -lelf
+DYNSO += -lz
 
-CCINCLUDE += -I/kernel-src/tools/testing/selftests/bpf
+CFLAGS += -static
+CFLAGS += -Wall
+CFLAGS += -o
 
-LOADINCLUDE += -I/kernel-src/samples/bpf
-LOADINCLUDE += -I/kernel-src/tools/lib
-LOADINCLUDE += -I/kernel-src/tools/perf
-LOADINCLUDE += -I/kernel-src/tools/include
-LIBRARY_PATH = -L/usr/local/lib64
-BPFSO = -lbpf
+$(TARGET): main.c
+	$(GCC) main.c $(INCLUDE) $(LIBRARY_PATH) $(DYNSO) $(CFLAGS) $(TARGET) -g
 
-# Setting -DHAVE_ATTR_TEST=0 for the kernel containing below patch:
-# 06f84d1989b7 perf tools: Make usage of test_attr__* optional for perf-sys.h
-#
-# The patch was included in Linus's tree starting v5.5-rc1, but was also included
-# in stable kernel branch linux-5.4.y. So it's hard to determine whether a kernel
-# is affected based on the kernel version alone:
-# - for a v5.4 kernel from Linus's tree, no;
-# - for a v5.4 kernel from the stable tree (used by many distros), yes.
-#
-# So let's look at the actual kernel source code to decide.
-#
-# See more context at:
-# https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=06f84d1989b7e58d56fa2e448664585749d41221
-# https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=fce9501aec6bdda45ef3a5e365a5e0de7de7fe2d
-
+.DEFAULT_GOAL := $(TARGET)
--- main.c
+++ main.c
@@ -6,44 +6,22 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-#include "/home/asss/projects/btfdemo/libbpf/src/btf.h"
-#include "/home/asss/projects/btfdemo/libbpf/src/bpf.h"
+#include "libbpf/src/btf.h"
+#include "libbpf/src/bpf.h"
 
-struct btf {
-	void *raw_data;
-	void *raw_data_swapped;
-	__u32 raw_size;
-	bool swapped_endian;
-	struct btf_header *hdr;
-
-	void *types_data;
-	size_t types_data_cap; /* used size stored in hdr->type_len */
-	__u32 *type_offs;
-	size_t type_offs_cap;
-	__u32 nr_types;
-	struct btf *base_btf;
-	int start_id;
-	int start_str_off;
-	void *strs_data;
-	struct strset *strs_set;
-	bool strs_deduped;
-	int fd;
-	int ptr_sz;
-};
-
-	static inline __u64
+static inline __u64
 ptr_to_u64 (const void* ptr)
 {
 	return (__u64)(unsigned long)ptr;
 }
 
-	static inline int
+static inline int
 sys_bpf (enum bpf_cmd cmd, union bpf_attr* attr, unsigned int size)
 {
 	return syscall (__NR_bpf, cmd, attr, size);
 }
 
-	static inline int
+static inline int
 sys_bpf_fd (enum bpf_cmd cmd, union bpf_attr* attr, unsigned int size)
 {
 	int fd;
@@ -52,21 +30,49 @@ sys_bpf_fd (enum bpf_cmd cmd, union bpf_attr* attr, unsigned int size)
 	return fd;
 }
 
-// BPF_{PROG,MAP}_GET_NEXT_ID (get prog/map id's)
-// BPF_{PROG,MAP}_GET_FD_BY_ID (get a prog/map fd)
-// BPF_OBJ_GET_INFO_BY_FD (get bpf_prog_info/bpf_map_info with btf_id)
+// BPF_{PROG,MAP}_GET_NEXT_ID (get map/map id's)
+// BPF_{PROG,MAP}_GET_FD_BY_ID (get a map/map fd)
+// BPF_OBJ_GET_INFO_BY_FD (get bpf_map_info/bpf_map_info with btf_id)
 // BPF_BTF_GET_FD_BY_ID (get btf_fd) 
 // BPF_OBJ_GET_INFO_BY_FD (get btf)          
 
 void
 print_bpf_btf_info(struct bpf_btf_info *info)
 {
-	printf("btf: %lld\n", info->btf);
+	if (info->name_len == 0) {
+		printf("name len is 0\n");
+		return ;
+	}
+	printf("btf: %p\n", (struct btf*)info->btf);
 	printf("btf_size: %d\n", info->btf_size);
 	printf("id: %d\n", info->id);
-	//printf("name: %s\n", (char*)info->name);
+	printf("name: %s\n", (char*)info->name);
 	printf("name_len: %d\n", info->name_len);
 	printf("kernel_btf: %d\n", info->kernel_btf);
+}
+
+void
+print_bpf_prog_info(struct bpf_prog_info *info)
+{
+	printf("bpf_prog_info\n");
+	printf("----------\n");
+	printf("name: %s\n", info->name);
+	printf("jited_prog_len: %d\n", info->jited_prog_len);
+	printf("xlated_prog_len: %d\n",	info->xlated_prog_len);
+	printf("----------\n");
+}
+
+void
+print_bpf_map_info(struct bpf_map_info *info)
+{
+  printf("bpf_map_info\n");
+	printf("----------\n");
+	printf("name: %s\n", info->name);
+	printf("id: %d\n", info->id);
+	printf("key_size: %d\n", info->key_size);
+	printf("value_size: %d\n", info->value_size);
+	printf("max_entries: %d\n", info->max_entries);
+	printf("----------\n");
 }
 
 struct btf*
@@ -75,38 +81,68 @@ get_bpf_btf_info(__u32 id)
 	return btf__load_from_kernel_by_id(id);
 }
 
-int
-main() 
+void
+do_prog(int id)
 {
-	int err = 0;
-	__u32 id = 0;
-	int fd = 0;
-	int iter = 2;
-	while (iter--) {
-		err = bpf_prog_get_next_id(id, &id);
+	int fd = bpf_prog_get_fd_by_id(id);
+	struct bpf_prog_info info = {};
+	__u32 len = sizeof(info);
 
-		struct btf* btf = get_bpf_btf_info(id);
-		printf("id: %d ", id);
-		fd = bpf_prog_get_fd_by_id(id);
-		printf("fd: %d ", fd);
+	bpf_obj_get_info_by_fd(fd, &info, &len);
+	printf("info len: %d\n", len);
+	print_bpf_prog_info(&info);
 
-		struct bpf_btf_info btf_info = {};
-		__u32 len = sizeof(btf_info);
-		err = bpf_obj_get_info_by_fd(fd, &btf_info, &len);
-		// printf("err: %d", err);
+	int btf_id = info.btf_id;
+	int btf_fd = bpf_btf_get_fd_by_id(btf_id);
 
-		print_bpf_btf_info(&btf_info);
+	struct bpf_btf_info btf_info = {};
+	__u32 btf_info_len = sizeof(btf_info);
+	bpf_obj_get_info_by_fd(btf_fd, &btf_info, &btf_info_len);
+	print_bpf_btf_info(&btf_info);
+	return ;
+}
 
-		if (err) {
-			if (errno == ENOENT) {
-				err = 0;
-				break;
-			}
-			//p_err("can't get next program: %s%s", strerror(errno),
-			//		errno == EINVAL ? " -- kernel too old?" : "");
-			err = -1;
-			break;
-		}
-		printf("\n");
+void
+do_map(int id)
+{
+	int fd = bpf_map_get_fd_by_id(id);
+	struct bpf_map_info info = {};
+	__u32 len = sizeof(info);
+
+	bpf_obj_get_info_by_fd(fd, &info, &len);
+	printf("info len: %d\n", len);
+	print_bpf_map_info(&info);
+
+	int btf_id = info.btf_id;
+	int btf_fd = bpf_btf_get_fd_by_id(btf_id);
+
+	struct bpf_btf_info btf_info = {};
+	__u32 btf_info_len = sizeof(btf_info);
+	bpf_obj_get_info_by_fd(btf_fd, &btf_info, &btf_info_len);
+	print_bpf_btf_info(&btf_info);
+	return ;
+}
+
+int
+main(int argc, char** argv) 
+{
+	// int id = 45;
+	// int id = 9;
+	if (argc < 3) {
+		return -1;
 	}
+		
+	int id = atoi(argv[2]);
+	struct btf* btf = btf__load_from_kernel_by_id(id);
+	printf("btf: %p\n", btf);
+	printf("id: %d\n", id);
+	
+	if (!strcmp(argv[1], "map")) {
+		do_map(id);
+	} else if (!strcmp(argv[1], "prog")) {
+		do_prog(id);
+	} else
+		;
+
+	return 0;
 }
